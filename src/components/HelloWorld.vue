e1a9599d7feffafe9b5e8f656166d405325837d4
mountainricardo <mountainricardo@gmail.com>
2019-12-06 21:23:32 +0100

    First Vue CLI project commit. 06.12.2019

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,79 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+        <h1>Listado de convocatorias</h1>
+        <ul id="calls">
+          <li v-bind:key="k" v-for="(call, k) in calls">
+            <h2>{{call.name}}</h2>
+            <p>{{ call.description }}</p>
+            <ul>
+              <li v-bind:key="i" v-for="(merit, i) in merits
+                .filter(x => x.callId === call.id)
+                .sort(function(a,b){return Number(a.name.split(' ')[1]) - Number(b.name.split(' ')[1])} )">
+                <h3>{{ merit.name }}</h3>
+              </li>
+            </ul>
+          </li>
+        </ul>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+<script>
+import axios from 'axios'
+// import mocks for templating
+import calls from '../mocks/calls'
+import merits from '../mocks/merits'
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      calls: [],
+      merits: [],
+      response: ''
+    }
+  },
+  mounted () {
+    axios({ method: 'GET',
+      'url': 'https://saviatest.azurewebsites.net/api/Calls',
+      'headers': { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' } })
+      .then(result => {
+        this.calls = result.data
+      // console.log('results', this.calls)
+      }, error => {
+      // force hardcoded mock data just for developing web layer
+        this.calls = calls
+        this.merits = merits
+        console.error(error)
+      })
+  },
+  methods: { }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-  margin: 40px 0 0;
+  width: 50%;
+  text-align: left;
+  padding: 1rem;
 }
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  &#calls{
+    &>li{
+      background-color: #ededed;
+      margin-bottom: 1rem;
+      padding: 1rem;
+      h2, p{
+        text-align: left;
+      }
+      & > ul > li {
+        width: 100%;
+        background-color: #9e9e9e;
+        margin-bottom: .5rem;
+      }
+    }
+  }
 }
 </style>
